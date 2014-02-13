ecd4d10a7eeba2edb3308f6f95e41a5156d0a1f6
Kyle Conroy <kyle@kyleconroy.com>
2014-02-13 15:02:34 -0800

    Highlight selected item

--- js/deckbrew.js
+++ js/deckbrew.js
@@ -6,8 +6,12 @@ var Cache = {};
 
 var Card = React.createClass({
   render: function() {
+    var name = "card";
+    if (this.props.selected.id == this.props.id) {
+      name = name + " active";
+    }
     return (
-      <div className="card">{this.props.name}</div>
+      <div className={name}>{this.props.name}</div>
     );
   }
 });
@@ -16,43 +20,50 @@ var CardSearchForm = React.createClass({
   getInitialState: function() {
     return {value: ''};
   },
+  handleSubmit: function(event) {
+    this.setState({value: ''});
+    this.props.onSearch([]);
+    return false;
+  },
   handleChange: function(event) {
     var query = event.target.value;
     this.setState({value: event.target.value});
-
     if (query == "") {
       return this.props.onSearch([]);
     }
-
     if (Cache[query] !== undefined) {
       return this.props.onSearch(Cache[query]); 
     }
-
     $.ajax({
       url: 'https://api.deckbrew.com/mtg/cards/typeahead?q=' + query,
       dataType: 'json',
       success: function(data) {
+        Cache[query] = data;
         this.props.onSearch(data);
       }.bind(this),
       error: function(xhr, status, err) {
         console.error("DeckBrew API", status, err.toString());
       }.bind(this)
     });
+    return true;
   },
   render: function() {
     var value = this.state.value;
     return (
-      <input tabindex="1" type="text" value={value}
-        onChange={this.handleChange} placeholder="Enter a card name">
-      </input>
+      <form onSubmit={this.handleSubmit}>
+        <input tabindex="1" type="text" value={value} ref="search"
+          placeholder="Enter a card name" onChange={this.handleChange}>
+        </input>
+      </form>
     );
   }
 });
 
 var CardList = React.createClass({
   render: function() {
+    var selected = this.props.selected;
     var cardNodes = this.props.cards.map(function (card) {
-      return <Card name={card.name}/>;
+      return <Card id={card.id} name={card.name} selected={selected}/>;
     });
     return (
       <div className="cards">
@@ -64,16 +75,20 @@ var CardList = React.createClass({
 
 var CardSearchBox = React.createClass({
   getInitialState: function() {
-    return {cards: []};
+    return {cards: [], selected: {id: ''}};
   },
   handleSearch: function(cards) {
-    this.setState({cards: cards});
+    if (cards.length > 0) {
+      this.setState({cards: cards, selected: cards[0]});
+    } else {
+      this.setState({cards: cards});
+    }
   },
   render: function() {
     return (
       <div class="box">
         <CardSearchForm onSearch={this.handleSearch}></CardSearchForm>
-        <CardList cards={this.state.cards}></CardList>
+        <CardList cards={this.state.cards} selected={this.state.selected}></CardList>
       </div>
     );
   }
